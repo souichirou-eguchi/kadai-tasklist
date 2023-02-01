64b722377e9ae458f5fb88ff27677f77f52ba5a9
souichirou.eguchi <soichiro.eguchi@fujiya-sc.co.jp>
2023-02-01 07:23:16 +0000

    Add Column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
+    
   end
   
   def new
@@ -18,29 +20,29 @@ class TasksController < ApplicationController
       flash[:success] = 'タスクが正常に追加されました'
       redirect_to @task
     else
-      task.now[:danger] = 'タスクの追加に失敗しました'
+      flash.now[:danger] = 'タスクの追加に失敗しました'
       render :new
     end
   end
   
   def edit
-    @task = Task.find(params[:id])
+    
   end
   
   def update
-    @task = Task.find(params[:id])
+    
     
     if @task.update(task_params)
       flash[:success] = 'タスクが正常に更新されました'
       redirect_to @task
     else
-      task.now[:danger] = 'タスクが正常に更新されませんでした'
+      flash.now[:danger] = 'タスクが正常に更新されませんでした'
       render :new
     end
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    
     @task.destroy
     
     flash[:success] = 'タスクの削除が完了しました。'
@@ -48,9 +50,14 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end  
   #Strong Param HTTPリクエストの送信に対するセキュリティ対策
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_26_052335) do
+ActiveRecord::Schema.define(version: 2023_01_31_083421) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
